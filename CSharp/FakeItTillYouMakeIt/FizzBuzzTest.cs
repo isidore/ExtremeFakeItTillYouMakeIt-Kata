5e4f37d4a3dafbbf5403af5de44a032289c7ec14
Llewellyn Falco <isidore@setgame.com>
2016-07-27 14:06:26 -0700

    C#

---
+++ CSharp/FakeItTillYouMakeIt/FizzBuzzTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzFake
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void TestFizzBuzz()
+        {
+            string expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+            Assert.AreEqual(expected, FizzBuzz.Print(20));
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public static string Print(int top)
+        {
+            string expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+            return expected;
+        }
+    }
+}
