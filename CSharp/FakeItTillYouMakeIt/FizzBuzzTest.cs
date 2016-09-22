b71fbe963ac308496df437cf84c68edb271fce48
Llewellyn Falco <isidore@setgame.com>
2016-09-22 10:19:52 -0700

    Add TestCounter

--- CSharp/FakeItTillYouMakeIt/FizzBuzzTest.cs
+++ CSharp/FakeItTillYouMakeIt/FizzBuzzTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using ApprovalTests.Utilities;
+using ApprovalUtilities.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FizzBuzzFake
 {
@@ -7,6 +10,14 @@ namespace FizzBuzzFake
     {
         [TestMethod]
         public void TestFizzBuzz()
+        {
+            var java = PathUtilities.LocateFileFromEnviormentPath("javaw.exe");
+            var counterDisplayJar = PathUtilities.GetAdjacentFile("CounterDisplay.jar");
+            TestCounter.ResetAndLaunch(java.FirstOrDefault(), counterDisplayJar);// Comment out after 1st run
+            TestCounter.Track(Check20);
+        }
+
+        private static void Check20()
         {
             string expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
             Assert.AreEqual(expected, FizzBuzz.Print(20));
