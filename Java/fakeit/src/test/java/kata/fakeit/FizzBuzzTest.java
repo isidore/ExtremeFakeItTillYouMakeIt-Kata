a06724ea98e7e89b853c0bdde22252f22d345aa0
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-12-10 00:56:12 +0000

    updated to junit 5 and jdk 15

--- Java/fakeit/src/test/java/kata/fakeit/FizzBuzzTest.java
+++ Java/fakeit/src/test/java/kata/fakeit/FizzBuzzTest.java
@@ -1,17 +1,14 @@
 package kata.fakeit;
+ 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-
-public class FizzBuzzTest 
-    extends TestCase
+public class FizzBuzzTest
 {
-    
+    @Test
     public void test()
     {
         String expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
-        assertEquals(expected ,FizzBuzz.print(20));
+       assertEquals(expected ,FizzBuzz.print(20));
     }
 }
